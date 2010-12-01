6ac07df89b43af3412aa2716cade6d9a17247136
angamuthu <gam.psna2006@gmail.com>
2010-12-01 11:40:59 +0530

    asdns

--- move.js
+++ move.js
@@ -1,19 +1,19 @@
 console.log("javascript started");
 $(document).ready(function(){
-        $(#movable-div).css({'top': '200px', 'left' : '300px'})
+        $('#movable-div').css({'top': '200px', 'left' : '300px'})
         .keydown(function(event){
             console.log("key down event");
+            var div = $('#movable-div').get(0);
             if(event.keyCode == 37){
-                this.offset().left -= 20;
+                div.offset().left = (div.offset().left - 20) ? (div.offset().left - 20) : 0;
             }
             if(event.keyCode == 39){
-                this.offset().left += 20;
+                div.offset().right = (div.offset().right - 20) ? (div.offset().right - 20) : 0;
+            if(event.keyCode == 38){
+                div.offset().top = (div.offset().top - 20) ? (div.offset().top - 20) : 0;
             }
-            if(event.keyCode == 37){
-                this.offset().left -= 20;
-            }
-            if(event.keyCode == 37){
-                this.offset().left -= 20;
+            if(event.keyCode == 40){
+                div.offset().left = (div.offset().left - 20) ? (div.offset().left - 20) : 0;
             }
         }
 });
